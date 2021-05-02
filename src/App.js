f001f8c7d809252888cd0fe873e36bce447a5282
Kushagra sharma <myselflks@yahoo.com>
2021-05-02 19:07:21 +0530

    Basic UI creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Post from './Post';
+import { useState } from 'react';
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      username: "hogg_word",
+      caption: "React rocks !!!",
+      imageurl: "https://learnwithshikha.com/wp-content/uploads/2020/11/z2xg2bpo.jpg"
+
+    },
+    {
+      username: "dcodein_official",
+      caption: "we make coding related content",
+      imageurl: "https://www.stoodnt.com/blog/wp-content/uploads/2020/08/Best-Study-Tips.jpg"
+    },
+    {
+      username: "Brodha V",
+      caption: "It was way too easy",
+      imageurl: "https://upload.wikimedia.org/wikipedia/commons/4/40/Brodha_V.jpg"
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {/* header */}
+      <div className="app__header">
+        <img alt="Instagram" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" />
+
+      </div>
+      {/* <h1>Hello React</h1> */}
+      {/* posts */}
+      
+      {
+        posts.map(post => {
+          return  <Post prp={post} />;
+        })
+      }
+      
+      {/* posts */}
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/Post.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Post.css'
+import { Avatar, Container } from '@material-ui/core';
+
+function Post({ prp }) {
+    return (
+        <Container className="post" maxWidth="sm">
+            {/* header -> avatar + username */}
+            <div className="post__header">    
+                <Avatar className="post__avatar"  alt="Kushagra" src="download.jpg" />
+                <h3>{prp.username}</h3>
+            </div>
+            
+            {/* image */}
+            <img className="post__image" src={prp.imageurl} alt="Cover"></img>
+
+            {/* username + caption */}
+            <h4 className="post__text"><strong>{prp.username}</strong>: {prp.caption}</h4>
+            
+        </Container>
+    )
+}
+
+export default Post
